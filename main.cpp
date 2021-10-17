1c16dc74e57efa82b40abd1a40e85a8e47b409e8
LIL-CREAMY <91923096+LIL-CREAMY@users.noreply.github.com>
2021-10-16 18:21:25 -0700

    Added comments

--- main.cpp
+++ main.cpp
@@ -36,6 +36,8 @@ float g_framesPerSecond{0.f};
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 
+//This calculates the direction of a ray based 
+//on the tau and theta value on the viewport
 glm::vec3 direction(float tau, float sigma, float focal){
   glm::vec3 w(0.f, 0.f, -1.f);
   glm::vec3 u(1.f, 0.f, 0.f);
@@ -45,22 +47,62 @@ glm::vec3 direction(float tau, float sigma, float focal){
   return dir;
 }
 
-ray generateRay(float tau, float sigma, const Camera& camera){
-  ray r;
+//this puts values in the point and direction of a ray instance
+Ray generateRay(float tau, float sigma, const Camera& camera){
+  Ray r();
   r.point= camera.eye;
   r.vec= direction(tau,sigma, 1.f);
 }
 //To-Do List:
 //hardcode camera coordinate frame
 
+//camera
 class Camera(){
   glm::vec3 eye(0.f, 0.f, 0.f);
-  camera();
-  camera(float x, float y, float z){
+  Camera();
+  Camera(float x, float y, float z){
     eye[0]=x;
     eye[1]=y;
     eye[2]=z;
   } 
+}
+
+void rayCast(const Camera& camera, float length, float height){
+  for(int x=0; x<length; x++){
+    for(int y=0; y<height; y++){
+      Ray r();
+      r= generateRay(x,y,camera);
+
+    }
+  }
+}
+
+//create a depth testing function
+/*
+
+*/
+
+//holds the RGBO values of an object
+class color(){
+  glm::vec4 rgb;
+  color(){
+    glm::vec4 rgb = glm::vec4(0.f,0.f,0.f,0.f);
+  }
+  color(float R, float G, float B, float O){
+    glm::vec4 rgb = glm::vec4(R,G,B,O);
+  }
+}
+
+//holds a point and a vector for direction adn an RGB value
+class Ray(){
+  glm::vec3 point(0.f, 0.f, 0.f);
+  glm::vec3 vec(0.f, 0.f, 0.f);
+  color RGB();
+  Ray();
+  Ray(vec3 p, vec3 v){
+    point = p;
+    vec = v;
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -169,34 +211,7 @@ keyCallback(GLFWwindow* _window, int _key, int _scancode,
   }
 }
 
-void rayCast(const Camera& camera, float length, float height){
-  for(int x=0; x<length; x++){
-    for(int y=0; y<height; y++){
-      
-    }
-  }
-}
 
-class color(){
-  glm::vec4 rgb;
-  color(){
-    glm::vec4 rgb = glm::vec4(0.f,0.f,0.f,0.f);
-  }
-  color(float R, float G, float B, float O){
-    glm::vec4 rgb = glm::vec4(R,G,B,O);
-  }
-}
-
-class ray(){
-  glm::vec3 point(0.f, 0.f, 0.f);
-  glm::vec3 vec(0.f, 0.f, 0.f);
-  color RGB();
-  ray();
-  ray(vec3 p, vec3 v){
-    point = p;
-    vec = v;
-  }
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Main
